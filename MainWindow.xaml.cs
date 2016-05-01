2032751bc809a69b5698376aedc7a88e84384479
FoldZero <pplatinumss@gmail.com>
2016-05-02 00:44:38 +0100

    Initial

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Management;
+using System.Management.Instrumentation;  
+
+namespace StartupMode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ManagementClass mangnmt = new ManagementClass("Win32_StartupCommand");
+            ManagementObjectCollection mcol = mangnmt.GetInstances();
+            foreach (ManagementObject strt in mcol)
+            {
+                //listView1.Items.Add(strt);
+                Console.WriteLine(strt);
+                listView1.Items.Add(strt["Name"].ToString());
+            } 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
